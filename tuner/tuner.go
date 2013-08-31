58310929cda10fd9b4b5bd05893d2b227da17d3e
Marty Schoch <marty.schoch@gmail.com>
2013-08-31 18:11:29 -0400

    making tuner interactive

--- tuner/tuner.go
+++ tuner/tuner.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/mschoch/go-si4703"
 )
@@ -19,7 +23,7 @@ func main() {
 	fmt.Printf("init done\n")
 
 	fmt.Printf("setting volume\n")
-	d.SetVolume(10)
+	d.SetVolume(5)
 
 	fmt.Printf("trying to tune 101.1\n")
 	d.SetChannel(1011)
@@ -31,4 +35,31 @@ func main() {
 
 	fmt.Printf("disabled mute")
 	d.DisableMute()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		command := strings.SplitN(line, " ", 2)
+		switch command[0] {
+		case "quit":
+			os.Exit(0)
+		case "volume":
+			if len(command) > 1 {
+				val, err := strconv.Atoi(command[1])
+				if err != nil || val < 0 || val > 15 {
+					fmt.Printf("Invalid volume level, must be (0-15)")
+				}
+				d.SetVolume(uint16(val))
+			} else {
+				fmt.Printf("specify a volume level (0-15)\n")
+			}
+
+		}
+
+	}
 }
