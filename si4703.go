7c6dda1976fd53a1181100a298419d5014e1050c
Marty Schoch <marty.schoch@gmail.com>
2013-10-04 23:29:57 -0400

    more log

--- si4703.go
+++ si4703.go
@@ -614,6 +614,7 @@ func (d *Device) PollRDS() {
 				// rv = rv + fmt.Sprintf("Program Type: %d\n", d.registers[RDSB]>>5&0x1F)
 				//fmt.Printf("%s", rv)
 				d.rdsinfo.Update(d.registers[RDSA], d.registers[RDSB], d.registers[RDSC], d.registers[RDSD])
+				log.Printf("%v\n", d.rdsinfo)
 			}
 		}
 	}
